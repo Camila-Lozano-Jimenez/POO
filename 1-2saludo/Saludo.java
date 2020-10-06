366df7dfa3f09ed6448a484f396bbbc28ce2a55e
repl.it user <replituser@example.com>
2020-10-06 18:35:58 +0000

    El 1

--- 1-2saludo/Saludo.java
+++ 1-2saludo/Saludo.java
@@ -7,7 +7,7 @@ import javax.swing.JOptionPane;
 public class Saludo {
 
 	public static void main (String args[]) {		
-		String nombre = JOptionPane.showInputDialog(null,"Ingresa tu nombre");
-		JOptionPane.showMessageDialog(null,"Hola "+nombre);
+		String nom = JOptionPane.showInputDialog(null,"Ingresa tu nombre");
+		JOptionPane.showMessageDialog(null,"Hola "+nom);
 	}
 }
